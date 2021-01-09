809bc63943f9318adb1f72f09c858d6b3265d6c0
Ghulamrijal <ghulamrijal@gmail.com>
2021-01-09 11:38:48 +0700

    date

--- kalender.php
+++ kalender.php
@@ -102,13 +102,13 @@
                                 <div class="account-wrap">
                                     <div class="account-item clearfix js-item-menu">
                                         <div class="content">
-                                            <a class="js-acc-btn" href="#"></a>
+                                            <a class="js-acc-btn" href="#">USER</a>
                                         </div>
                                         <div class="account-dropdown js-dropdown">
                                             <div class="info clearfix">
                                                 <div class="content">
                                                     <h5 class="name">
-                                                        <a href="#">john doe</a>
+                                                        <a href="#">USER</a>
                                                     </h5>
                                                     <span class="email"></span>
                                                 </div>
@@ -121,10 +121,6 @@
                                                 <div class="account-dropdown__item">
                                                     <a href="#">
                                                         <i class="zmdi zmdi-settings"></i>Setting</a>
-                                                </div>
-                                                <div class="account-dropdown__item">
-                                                    <a href="#">
-                                                        <i class="zmdi zmdi-money-box"></i>Billing</a>
                                                 </div>
                                             </div>
                                             <div class="account-dropdown__footer">
